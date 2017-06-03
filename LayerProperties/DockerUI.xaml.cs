ca71f734a51fe69817d3c37902ad670f08914561
Máté Péter Surányi <suranyimpeter@gmail.com>
2017-06-03 17:33:11 +0200

    Add project files.

---
+++ LayerProperties/DockerUI.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Corel.Interop.VGCore;
+
+namespace LayerProperties
+{
+
+    public partial class DockerUI : UserControl
+    {
+        // Type definitions
+        private Corel.Interop.VGCore.Application cdrApplication;
+
+        private bool reverseSearch = false;
+
+        public DockerUI(Corel.Interop.VGCore.Application application)
+        {
+            this.cdrApplication = application;
+            InitializeComponent();
+
+            foreach (Layer l in this.cdrApplication.ActivePage.Layers )
+            {
+                lbLayernames.Items.Add(l.Name);
+            }
+        }
+
+    }
+}
